3a81cf2daa7c29b63dded16069a1309fd1b6eff2
youbaidjis <youba.etud@gmail.com>
2022-08-19 00:43:37 -0700

    Completed assignment

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ApplicationRecord
 
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
   def to_s
     "#{first_name} #{last_name}"
   end
